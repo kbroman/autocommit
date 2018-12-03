b0cc93c7a0e6b4849dea1a4db53d60126abd8df4
Karl Broman <kbroman@gmail.com>
2018-12-04 04:53:41 +0530

    Add a comment at the top

--- autocommit.rb
+++ autocommit.rb
@@ -1,4 +1,15 @@
 #!/usr/bin/env ruby
+##############################
+# automatically commit a file every x seconds
+#
+# run like:
+#     autocommit.rb [filename] [x]
+#
+# if x is missing or <2, we use 2
+#
+# You need to be in the git repository that contains the file,
+# and you need to have already set up a remote
+
 
 require 'date'
 
