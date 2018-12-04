9a5ecea4168513d76a05f0aa2dd1d0330ab8eabd
Karl Broman <kbroman@gmail.com>
2018-12-04 08:40:02 +0530

    Revise the documentation comment

--- autocommit.rb
+++ autocommit.rb
@@ -3,9 +3,11 @@
 # automatically commit a file every x seconds
 #
 # run like:
-#     autocommit.rb [filename] [x]
+#     autocommit.rb [filename] [n]
 #
-# if x is missing or <2, we use 2
+# where [filename] is the file to repeatedly commit and push
+# and n is the number of seconds to delay between attempts.
+# If n is missing, we use 10 sec; if n<2 we use 2.
 #
 # You need to be in the git repository that contains the file,
 # and you need to have already set up a remote
